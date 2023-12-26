9dcd6bae25817151ae1a4e451c31a4afb9c8701e
slavcho179 <slavidimitrov06@abv.bg>
2023-12-26 02:07:27 +0200

    del test file

--- EnglishWordsScript/Development/__main__.py
+++ EnglishWordsScript/Development/__main__.py
@@ -1,7 +1,6 @@
-def write_sentences():
+def write_sentences() -> bool:
     """
-    This function has offers 3 main functionalities.
-    They are:
+    This function has 3 main functionalities.
     1-st: Save the words from (words text file) into a list.
     2-nd: Allows you to write a sentence for every word and it automatically
     saves it into (sentences text file).
@@ -24,7 +23,7 @@ def write_sentences():
         if line:  # if it's not a blank line
             # Split the line by ("-") in order to get the word and its definition
             word, definition = line.split('-')
-            print(f'Word or a phrase found: {word}')
+            print(f'\nWord or a phrase found: {word}')
             print(f'Definition: {definition}')
 
             # Collect sentences into a list
@@ -47,7 +46,7 @@ def write_sentences():
 
     # Save the sentences into a text file
     file = open('./sentences_list.txt', 'a')
-    file.write('\n')
+
     file.write('\n'.join(sentences))
     file.close()
 
@@ -59,22 +58,25 @@ def write_sentences():
     return True
 
 
-def access_dictionary():
+def access_dictionary() -> bool:
     f = open('./dictionary.txt', 'r')
     data = f.read()
     print(data)
     f.close()
+    return True
 
 
-def show_new_words():
+def show_new_words() -> bool:
     f = open('./list_of_words.txt', 'r')
     data = f.read()
     if data:
         print('\nList of all new words:')
         print(data)
         f.close()
+        return True
     else:
         print('There are not any new words.')   # ADD A OPERATION - ADDING NEW WORDS WHEN THE LIST IS EMPTY
+        return True
 
 
 def test_knowledge():
@@ -82,24 +84,42 @@ def test_knowledge():
 
 
 def show_info():
-    ...
+    message = '\tEverything you need to know\n' \
+              'This program has been born in my head and it was implemented into code by my own hands.\n' \
+              '  The mission:' \
+              'To help others. Learning never stops and it makes us better humans. This was the main purpose,\n' \
+              'for which I managed to create something, that will help us to be and earn more!/n' \
+              '  How to use it?\n' \
+              'Everything in this program is separated into sections.\n' \
+              ' Let\'s say that you\'d just inputted the words, that you want to write,\n' \
+              'into the (list of words txt) This is when you want to choose (1).\n' \
+              ' On the other hand, when you had already used this program at least once.\n' \
+              'You\'re not sure which words you have in your text file, choose (2) and find out.\n'\
+              ' Moreover, if you want to see which words you have already learned input (3)\n' \
+              '\n' \
+              ' For the forth operation (4) you are not only going to learn. You\'re going to be challenged\n' \
+              'Choose it and go to face your demons or stay the same forever!\n' \
+              ' (5) is what you had already chosen, so I believe it does not need and explanation\n' \
+              ' If information is all you needed at this point choose (6) to exit the temple.\n' \
+              'I will be waiting for your return, because I hope that your learning will be endless process!'
 
 
-def menu():
+def menu() -> str:
     """
     This function will contain only the user menu
     """
-    print('Please, choose an operation (1/2/3/4/5/6):\n'
-          '1. Write down new sentences\n'
-          '2. See the new words\n'
-          '3. Open the dictionary\n'
-          '4. Test your knowledge\n'
-          '5. Info\n'
-          '6. Exit the program')
+    menu_message = ('Please, choose an operation (1/2/3/4/5/6):\n'
+                    '1. Write down new sentences\n'
+                    '2. See the new words\n'
+                    '3. Open the dictionary\n'
+                    '4. Test your knowledge\n'
+                    '5. Info\n'
+                    '6. Exit the program')
+    return menu_message
 
 
 # This and the other 2 functions below are responsible for the input
-def get_input():
+def get_input() -> str:
     """
     When called: This function prints out a message, which asks the user to input a single number.
     Then: Check if user's input is valid by calling the (input validator) function and if its not -
@@ -142,7 +162,7 @@ def main():
     This function contains the  of the entire program.
     It will call other functions when the user needs them.
     """
-    menu()
+    print(menu())
     choice = get_input()
     while True:
         if choice == 'm':
@@ -157,9 +177,9 @@ def main():
         elif choice == 4:
             test_knowledge()
         elif choice == 5:
-            show_info()
+            print(show_info())
         elif choice == 6:
-            test_knowledge()
+            break
         choice = get_input()
 
 
@@ -169,9 +189,11 @@ print(' Hello, Learner!\n'
 if __name__ == '__main__':
     main()
 
-print('Thank yourself for the time you spent learning!\n'
-      'We are so happy that you just used our program!\n'
-      'If you had detected any bugs or if you just have any ideas\n'
-      'how we should improve our app, please mail us here:\n'
-      '\tslavidimitrov54@gmail.com')
+print('\nThank yourself for the time you spent learning!\n'
+      'I am so happy that you just used my program!\n'
+      'If you had seen any bugs or if you have any ideas\n'
+      'how I should improve my app, please mail me here:\n'
+      ' -slavidimitrov54@gmail.com\n'
+      '                             Best wishes,\n'
+      '                             Slavi Dimitrov')
 
